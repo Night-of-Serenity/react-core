8042a3c642f9ed07e5f0ea4dc2c5c9ac1a81c682
Nightz <peerapat.ud@gmail.com>
2023-04-25 01:11:47 +0700

    Add comments and change name of gameArray into gameMetrix

--- 09-Component Communication/ws-tic-tac-toe/main.js
+++ 09-Component Communication/ws-tic-tac-toe/main.js
@@ -1,24 +1,26 @@
-function GameBoard({ gameArray, setGameArray, player, setPlayer, winner, setWinner, countWinX, countWinO, setCountWinX, setCountWinO }) {
+function GridTable({ gameMetrix, setGameMetrix, player, setPlayer, winner, setWinner, countWinX, countWinO, setCountWinX, setCountWinO }) {
+  // Functin for handle event when click element inside game table
   function handleTick(e, indexX, indexY) {
-    console.log("gameArray:", gameArray);
-    if (gameArray[indexX][indexY] === "" && winner === null) {
+    // When click on empty element inside table and there is no winner, take turn set string 'X' or 'O' depend on player state with that clicked element's 'index X' and 'index Y' inside gameMetrix
+    console.log("gameMetrix:", gameMetrix);
+    if (gameMetrix[indexX][indexY] === "" && winner === null) {
       if (player === true) {
-        const newArray = gameArray.slice();
+        const newArray = gameMetrix.slice();
         newArray[indexX][indexY] = "X";
-        setGameArray(newArray);
+        setGameMetrix(newArray);
         setPlayer(!player);
-        const transformX = tranformMetrix(gameArray, "X");
+        const transformX = tranformMetrix(gameMetrix, "X");
         console.log("transformX:", transformX);
         if (checkWin(transformX)) {
           setWinner("X");
           setCountWinX(countWinX + 1);
         }
       } else {
-        const newArray = gameArray.slice();
+        const newArray = gameMetrix.slice();
         newArray[indexX][indexY] = "O";
-        setGameArray(newArray);
+        setGameMetrix(newArray);
         setPlayer(!player);
-        const transformO = tranformMetrix(gameArray, "O");
+        const transformO = tranformMetrix(gameMetrix, "O");
         console.log("transformO:", transformO);
         if (checkWin(transformO)) {
           setWinner("O");
@@ -28,86 +30,84 @@ function GameBoard({ gameArray, setGameArray, player, setPlayer, winner, setWinn
     }
   }
   return (
-    <div className="boardgame">
-      <h3>tic-tac-toe</h3>
-      <div className="game-table">
-        <div
-          className="box-00"
-          onClick={(e) => {
-            handleTick(e, 0, 0);
-          }}
-        >
-          {gameArray[0][0]}
-        </div>
-        <div
-          className="box-01"
-          onClick={(e) => {
-            handleTick(e, 0, 1);
-          }}
-        >
-          {gameArray[0][1]}
-        </div>
-        <div
-          className="box-02"
-          onClick={(e) => {
-            handleTick(e, 0, 2);
-          }}
-        >
-          {gameArray[0][2]}
-        </div>
-        <div
-          className="box-03"
-          onClick={(e) => {
-            handleTick(e, 1, 0);
-          }}
-        >
-          {gameArray[1][0]}
-        </div>
-        <div
-          className="box-04"
-          onClick={(e) => {
-            handleTick(e, 1, 1);
-          }}
-        >
-          {gameArray[1][1]}
-        </div>
-        <div
-          className="box-05"
-          onClick={(e) => {
-            handleTick(e, 1, 2);
-          }}
-        >
-          {gameArray[1][2]}
-        </div>
-        <div
-          className="box-06"
-          onClick={(e) => {
-            handleTick(e, 2, 0);
-          }}
-        >
-          {gameArray[2][0]}
-        </div>
-        <div
-          className="box-07"
-          onClick={(e) => {
-            handleTick(e, 2, 1);
-          }}
-        >
-          {gameArray[2][1]}
-        </div>
-        <div
-          className="box-08"
-          onClick={(e) => {
-            handleTick(e, 2, 2);
-          }}
-        >
-          {gameArray[2][2]}
-        </div>
+    <div className="game-grid">
+      <div
+        className="box-00"
+        onClick={(e) => {
+          handleTick(e, 0, 0);
+        }}
+      >
+        {gameMetrix[0][0]}
+      </div>
+      <div
+        className="box-01"
+        onClick={(e) => {
+          handleTick(e, 0, 1);
+        }}
+      >
+        {gameMetrix[0][1]}
+      </div>
+      <div
+        className="box-02"
+        onClick={(e) => {
+          handleTick(e, 0, 2);
+        }}
+      >
+        {gameMetrix[0][2]}
+      </div>
+      <div
+        className="box-03"
+        onClick={(e) => {
+          handleTick(e, 1, 0);
+        }}
+      >
+        {gameMetrix[1][0]}
+      </div>
+      <div
+        className="box-04"
+        onClick={(e) => {
+          handleTick(e, 1, 1);
+        }}
+      >
+        {gameMetrix[1][1]}
+      </div>
+      <div
+        className="box-05"
+        onClick={(e) => {
+          handleTick(e, 1, 2);
+        }}
+      >
+        {gameMetrix[1][2]}
+      </div>
+      <div
+        className="box-06"
+        onClick={(e) => {
+          handleTick(e, 2, 0);
+        }}
+      >
+        {gameMetrix[2][0]}
+      </div>
+      <div
+        className="box-07"
+        onClick={(e) => {
+          handleTick(e, 2, 1);
+        }}
+      >
+        {gameMetrix[2][1]}
+      </div>
+      <div
+        className="box-08"
+        onClick={(e) => {
+          handleTick(e, 2, 2);
+        }}
+      >
+        {gameMetrix[2][2]}
       </div>
     </div>
   );
 }
 
+// Function use for check if input of transformed matrix of numbers 1 satified win condition
 function checkWin(metrix) {
   function sumArr(arr) {
     return arr.reduce((a, c) => a + c, 0);
@@ -151,6 +151,8 @@ function checkWin(metrix) {
   return false;
 }
 
+// function for tranform matrix of string 'X' or 'O' into matrix of number 1
+// 'userInput' parameter use for accept game matrix of 'X' and 'O', 'user' parameter can put string 'X' or 'O' as input
 function tranformMetrix(userInput, user) {
   let tranformResult = [];
   for (let rowIndex in userInput) {
@@ -168,19 +170,21 @@ function tranformMetrix(userInput, user) {
 }
 
 function App() {
-  const [winner, setWinner] = React.useState(null);
-  const [countWinX, setCountWinX] = React.useState(0);
-  const [countWinO, setCountWinO] = React.useState(0);
+  const [winner, setWinner] = React.useState(null); // winner keep winner of game as 'X' or 'O', initialize as null
+  const [countWinX, setCountWinX] = React.useState(0); // use for count how many times X win
+  const [countWinO, setCountWinO] = React.useState(0); // use for count how many times O win
 
-  const [gameArray, setGameArray] = React.useState([
+  const [gameMetrix, setGameMetrix] = React.useState([
+    // table matrix of boardgame keep string 'X' or 'O' or empty '' inside matrix 3x3
     ["", "", ""],
     ["", "", ""],
     ["", "", ""],
   ]);
-  const [player, setPlayer] = React.useState(true);
+  const [player, setPlayer] = React.useState(true); // use to classify 2 players, 'true' for player X and 'false' for player O
 
   function handleReset() {
-    setGameArray([
+    // event function for "reset button"
+    setGameMetrix([
       ["", "", ""],
       ["", "", ""],
       ["", "", ""],
@@ -190,19 +194,24 @@ function App() {
 
   return (
     <div className="container">
-      <GameBoard
-        gameArray={gameArray}
-        setGameArray={setGameArray}
-        player={player}
-        setPlayer={setPlayer}
-        winner={winner}
-        setWinner={setWinner}
-        countWinX={countWinX}
-        countWinO={countWinO}
-        setCountWinX={setCountWinX}
-        setCountWinO={setCountWinO}
-      />
+      <div className="boardgame">
+        <h3>tic-tac-toe</h3>
+        {/* component function use for generate grid table of game*/}
+        <GridTable
+          gameMetrix={gameMetrix}
+          setGameMetrix={setGameMetrix}
+          player={player}
+          setPlayer={setPlayer}
+          winner={winner}
+          setWinner={setWinner}
+          countWinX={countWinX}
+          countWinO={countWinO}
+          setCountWinX={setCountWinX}
+          setCountWinO={setCountWinO}
+        />
+      </div>
       <button onClick={handleReset}>Reset Game</button>
+      {/* display all status of game */}
       <div className="game-status">
         <div className="winner">{winner === "X" ? "Winner is X" : winner === "O" ? "Winner is O" : "No Winner"}</div>
         <div style={{ color: player === true ? "red" : "blue" }} className="player-status">
